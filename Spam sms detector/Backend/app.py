2bf6addf2189bdeb05cd525a98c91da327e59ea8
Huzaifa3242 <151153541+Huzaifa3242@users.noreply.github.com>
2025-04-20 17:11:45 +0500

    added index.html

--- Spam sms detector/Backend/app.py
+++ Spam sms detector/Backend/app.py
@@ -4,11 +4,13 @@ import nltk
 from nltk.corpus import stopwords
 import string
 from nltk.stem.porter import PorterStemmer
+from flask_cors import CORS
 
 # nltk.download('punkt')
 # nltk.download('stopwords')
 
 app = Flask(__name__)
+CORS(app)
 
 # Load the model and vectorizer
 tfidf = pickle.load(open('transform.pkl', 'rb'))
@@ -42,3 +44,9 @@ def predict():
 
 if __name__ == '__main__':
     app.run(debug=True)
+# Congratulations on completing the project successfully!
+# We’re excited to have you on board for the next phase.
+# Please let me know when you’re available to meet.
+# You have been selected to receive $1000 cash!
+# Click here to claim your money right now.
+# This offer is valid for a limited time only
